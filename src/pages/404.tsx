6442b302678465a82245d06db9f940c37aa19405
Callum Henry <callum.henry@rawcore101.com>
2020-04-08 16:13:26 +0100

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import React from "react"
+import { jsx } from "theme-ui"
 import Layout from "../components/layout"
 import { SEOBlock } from "../components/SEO"
 import { Link } from "gatsby"
@@ -7,9 +9,14 @@ import "../scss/app.scss";
 const NotFoundPage = () => (
   <Layout>
     <SEOBlock title="404: Not found" />
+    <div sx={{
+          py: 3,
+          px: 4,
+          }}>
     <div className="container container--text">
       <h1>NOT FOUND</h1>
       <p>This page doesnt exist, please return to the <Link to="/">homepage</Link> and try again.</p>
+    </div>
     </div>
   </Layout>
 )
