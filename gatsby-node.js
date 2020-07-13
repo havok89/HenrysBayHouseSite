ed21b752c019a2bcbcee3aa6013a9cf7188d5dfd
Callum Henry <callum.henry@rawcore101.com>
2020-07-13 20:40:14 +0100

    fix menu page

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,3 +61,46 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type wordpress__PAGE implements Node {
+      acf: acf
+    }
+
+    type acf implements Node @dontInfer {
+      starters: [menuitem]
+      mains: [menuitem]
+      favourites: [menuitem]
+      grills: [menuitem]
+      sweets: [menuitem]
+      grills_text: String!
+      legend: String!
+      gatsby_template: String!
+      meta_description: String!
+      meta_title: String!
+      share_image: wordpress__wp_media
+      image_carousel: [carouselitem]
+      menugroup: [menugroup]
+    }
+
+    type menuitem implements Node @dontInfer {
+      title: String!
+      description: String!
+      price: String!
+      allergens: String!
+    }
+    
+    
+    type carouselitem implements Node @dontInfer {
+      link: String!
+      alt_text: String!
+      image: wordpress__wp_media
+    }
+    type menugroup implements Node @dontInfer {
+      menu_title: String!
+      menu_description: String!
+      menu_image: wordpress__wp_media
+    }
+  `)
+}
